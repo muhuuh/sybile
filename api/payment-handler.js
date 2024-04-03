49cc855df2df2145f645f5266eb55c1084e4c1e5
arani <aranimontes@protonmail.com>
2024-04-03 21:55:33 +0200

    corrected serverless function 2

--- api/payment-handler.js
+++ api/payment-handler.js
@@ -6,35 +6,40 @@ const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default async function handler(req, res) {
-  console.log("req");
-  console.log(req);
-  console.log(req.method);
-  console.log(res);
-  console.log("req.body: ", JSON.stringify(req.body, null, 2));
   console.log("Webhook triggered");
+  console.log("req.body: ", JSON.stringify(req.body, null, 2));
 
-  const { type, record } = req.body;
-  console.log("Type:", type);
   if (req.method === "POST") {
     // Parse the notification from Blocknative
-    console.log("serverless function");
-    console.log(req.body);
-    const { transaction } = req.body; // Adjust based on actual Blocknative notification payload
 
-    // Perform validation and update Supabase if necessary
-    const { data, error } = await supabase
-      .from("payment_info")
-      .update({ payment_received: true })
-      .match({ address_payer: transaction.from });
-    if (error) {
-      console.error("Supabase update error:", error);
-      return res.status(500).json({ error: "Failed to update database" });
+    const transaction = req.body.transactions[0];
+    console.log("Transaction details:", transaction);
+
+    try {
+      // Assuming 'payment_info' table has a column 'address_payer' to match 'from' address in the transaction
+      const { data, error } = await supabase
+        .from("payment_info")
+        .update({ payment_received: true })
+        .match({ address_payer: transaction.from });
+
+      if (error) {
+        console.error("Supabase update error:", error);
+        return res.status(500).json({ error: "Failed to update database" });
+      }
+
+      return res
+        .status(200)
+        .json({ message: "Transaction processed successfully", details: data });
+    } catch (error) {
+      console.error("Processing error:", error);
+      return res
+        .status(500)
+        .json({ error: "Error processing the transaction" });
     }
 
     //if successful, get request_id from this row, and with this request id, update analysis_request table
-
-    return res.status(200).json({ message: "Transaction processed" });
   } else {
+    console.log("Method not allowed");
     return res.status(405).json({ error: "Method not allowed" });
   }
 }
