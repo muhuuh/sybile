5208114a7c29eda55f5309d0b6257c93a3c828e0
arani <aranimontes@protonmail.com>
2024-04-03 22:18:11 +0200

    update key access supabase

--- api/payment-handler.js
+++ api/payment-handler.js
@@ -2,7 +2,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default async function handler(req, res) {
