46fd8c9f39ca281045003c77862410011c2a296b
arani <aranimontes@protonmail.com>
2024-04-06 12:36:16 +0200

    tab active also with empty url

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,34 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 const Header = () => {
+  const location = useLocation(); // Get the current location
+  const isAnalysisActive =
+    location.pathname === "/" || location.pathname === "/main";
+
   return (
     <header className="bg-white border-b py-8 flex justify-between items-center px-24 shadow-md">
       <NavLink
         to="/main"
-        className="text-3xl font-bold tracking-widest text-indogoDye"
+        className="text-3xl font-bold tracking-widest text-indigoDye"
       >
         ManyMe
       </NavLink>
       <div className="flex text-gray-600 text-xl justify-center gap-x-20 tracking-wider">
         <NavLink
           to="/main"
-          className={({ isActive }) =>
-            isActive
+          className={`${
+            isAnalysisActive
               ? "underline decoration-salmon font-bold"
-              : "" + " text-gray-700"
-          }
+              : "text-gray-700"
+          }`}
         >
           Analysis
         </NavLink>
         <NavLink
           to="/demo"
           className={({ isActive }) =>
-            isActive
-              ? "underline decoration-salmon font-bold"
-              : "" + " text-gray-700"
+            isActive ? "underline decoration-salmon font-bold" : "text-gray-700"
           }
         >
           Demo
@@ -34,9 +36,7 @@ const Header = () => {
         <NavLink
           to="/faq"
           className={({ isActive }) =>
-            isActive
-              ? "underline decoration-salmon font-bold"
-              : "" + " text-gray-700"
+            isActive ? "underline decoration-salmon font-bold" : "text-gray-700"
           }
         >
           FAQ
